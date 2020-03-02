42afab725d82335423f8ea4b4a194cca47a2be07
Peter Odding <peter@peterodding.com>
2020-03-02 21:23:20 +0100

    Define default server port

--- pdiffcopy/__init__.py
+++ pdiffcopy/__init__.py
@@ -1,7 +1,7 @@
 # Fast synchronization of large files inspired by rsync.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: February 29, 2020
+# Last Change: March 2, 2020
 # URL: https://pdiffcopy.readthedocs.io
 
 """Configuration defaults for the ``pdiffcopy`` program."""
@@ -17,3 +17,6 @@ BLOCK_SIZE = 1024 * 1024
 
 DEFAULT_CONCURRENCY = int(max(2, multiprocessing.cpu_count() / 3.0))
 """The default concurrency to be used by ``pdiffcopy`` (at least two, at most 1/3 of available cores)."""
+
+DEFAULT_PORT = 8080
+"""The default port number for the ``pdiffcopy`` server (an integer number, defaults to 8080)."""
