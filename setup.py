430241078633c653e53222ca42fc6ef4b696ee2d
Peter Odding <peter@peterodding.com>
2020-03-06 22:13:56 +0100

    Update setup.py trove classifiers (and apply Black, with minor adjustments)

--- setup.py
+++ setup.py
@@ -30,15 +30,15 @@ from setuptools import find_packages, setup
 
 def get_contents(*args):
     """Get the contents of a file relative to the source distribution directory."""
-    with codecs.open(get_absolute_path(*args), 'r', 'UTF-8') as handle:
+    with codecs.open(get_absolute_path(*args), "r", "UTF-8") as handle:
         return handle.read()
 
 
 def get_version(*args):
     """Extract the version number from a Python module."""
     contents = get_contents(*args)
-    metadata = dict(re.findall('__([a-z]+)__ = [\'"]([^\'"]+)', contents))
-    return metadata['version']
+    metadata = dict(re.findall("__([a-z]+)__ = ['\"]([^'\"]+)", contents))
+    return metadata["version"]
 
 
 def get_requirements(*args):
@@ -47,10 +47,10 @@ def get_requirements(*args):
     with open(get_absolute_path(*args)) as handle:
         for line in handle:
             # Strip comments.
-            line = re.sub(r'^#.*|\s#.*', '', line)
+            line = re.sub(r"^#.*|\s#.*", "", line)
             # Ignore empty lines
             if line and not line.isspace():
-                requirements.add(re.sub(r'\s+', '', line))
+                requirements.add(re.sub(r"\s+", "", line))
     return sorted(requirements)
 
 
@@ -60,48 +60,51 @@ def get_absolute_path(*args):
 
 
 setup(
-    name='pdiffcopy',
-    version=get_version('pdiffcopy', '__init__.py'),
+    name="pdiffcopy",
+    version=get_version("pdiffcopy", "__init__.py"),
     description="Fast synchronization of large files inspired by rsync",
-    long_description=get_contents('README.rst'),
-    url='https://pdiffcopy.readthedocs.io',
+    long_description=get_contents("README.rst"),
+    url="https://pdiffcopy.readthedocs.io",
     author="Peter Odding",
-    author_email='peter@peterodding.com',
-    license='MIT',
+    author_email="peter@peterodding.com",
+    license="MIT",
     packages=find_packages(),
-    entry_points=dict(console_scripts=[
-        'pdiffcopy = pdiffcopy.cli:main',
-    ]),
-    install_requires=get_requirements('requirements.txt'),
+    entry_points=dict(console_scripts=["pdiffcopy = pdiffcopy.cli:main"]),
+    install_requires=get_requirements("requirements.txt"),
     extras_require=dict(
-        client=get_requirements('requirements-client.txt'),
-        server=get_requirements('requirements-server.txt'),
+        client=get_requirements("requirements-client.txt"),
+        server=get_requirements("requirements-server.txt"),
     ),
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Communications',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Shells',
-        'Topic :: System :: System Shells',
-        'Topic :: System :: Systems Administration',
-        'Topic :: Terminals',
-        'Topic :: Utilities',
-    ])
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Communications",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Shells",
+        "Topic :: System :: System Shells",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ],
+)
